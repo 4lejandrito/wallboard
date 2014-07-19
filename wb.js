c86ed045534f1e1d88d032907b0499f796063dc9
Alejandro Tardín <alejandro@tardin.com>
2014-07-19 01:39:29 +0100

    Better organization and styles

---
+++ wb.js
@@ -0,0 +1,20 @@
+angular.module('wb', [])
+
+.controller('wb', function($scope, $timeout) {
+    $scope.widgets = [{name: 'builds', w: 19, h: 5}, {name: 'heroes', w: 8, h: 5}];
+
+    $timeout(function() {    
+        $(".gridster > ul").gridster({
+            widget_margins: [10, 10], 
+            widget_base_dimensions: [30, 30],					
+            resize: {
+                enabled: true
+            }
+        }).data('gridster');  
+        $scope.loaded = true;
+    }, 0);
+}).controller('widget', function($scope, $http) {
+    $scope.get = function(url, data) {
+        return $http.get('widgets/' + $scope.widget.name + '/' + url, data);
+    }
+});       
