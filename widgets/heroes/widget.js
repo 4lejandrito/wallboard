ebddcef30b5f9f56b8742ff31ff5ba89cefa870f
Alejandro Tardín <alejandro@tardin.com>
2014-07-20 02:08:15 +0100

    Style improvements

--- widgets/heroes/widget.js
+++ widgets/heroes/widget.js
@@ -1,3 +1,3 @@
 angular.module('wb').controller('heroes', function($scope, $http) {
-    $scope.heroes = [{}, {}, {}, {}, {}, {}, {}, {}];
+    $scope.heroes = [{name: 'Alejandro Tardin', commits: 89}, {name: 'Pau Folque', commits: 88}, {name: 'Kingsley Bickle', commits: 87}];
 });
