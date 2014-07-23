63ad0c9035e4714fc74af8787fafecb0a529aba3
Alejandro Tardín <alejandro@tardin.com>
2014-07-24 00:00:12 +0100

    Backend in ruby

---
+++ rbackend/config.ru
@@ -0,0 +1,3 @@
+require_relative "wallboard.rb"
+
+run Wallboard::API
---
+++ rbackend/plugins/builds/plugin.rb
@@ -0,0 +1,19 @@
+require 'travis'
+
+module Builds
+    class Main < Wallboard::Plugin
+        def get
+            client = Travis::Client.new
+            client.github_auth('1f5fe06e1616dacf11606678b841b527d5b4015a')
+            builds = Travis::Repository.find_all(member: '4lejandrito').map {|repo| repo.last_build()}.compact            
+            builds.map do |build|
+                {
+                    'name' => build.repository.name,
+                    'status' => {'green' => 0, 'yellow' => 1, 'red' => 2}[build.color],
+                    'commit' => build.commit.message,
+                    'authors' => [{'name' => build.commit.author_name, 'email' => build.commit.author_email}]
+                }
+            end    
+        end
+    end
+end
