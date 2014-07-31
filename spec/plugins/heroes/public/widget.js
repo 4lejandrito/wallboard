8919e220c0f83c70b99384b6b5f8b433d84953cd
Alejandro Tardín <alejandro@tardin.com>
2014-07-31 08:20:21 +0100

    Assets

--- spec/plugins/heroes/public/widget.js
+++ spec/plugins/heroes/public/widget.js
@@ -1,6 +1,1 @@
-angular.module('wb').service('heroes', function($http) {
-    this.start = function($scope, done) {
-        $scope.heroes = [{name: 'Alejandro Tardin', commits: 89}, {name: 'Pau Folque', commits: 88}, {name: 'Kingsley Bickle', commits: 87}];
-        done();
-    }
-});
+js
