f2f894b923f222d36cd000f0b4513b257cd22e0f
Alejandro Tardín <alejandro@tardin.com>
2014-10-10 09:00:25 +0100

    WIP

--- lib/wallboard/plugins/heroes/plugin.rb
+++ lib/wallboard/plugins/heroes/plugin.rb
@@ -3,7 +3,11 @@ require 'wallboard/plugin'
 module Heroes
     class Main < Wallboard::Plugin
         def get
-            [{:name => 'Alejandro Tardin', :commits => 89}, {:name =>'Pau Folque', :commits => 88}, {:name => 'Kingsley Bickle', :commits => 87}]
+            [
+                {:name => 'Alejandro Tardin', :email => 'alejandro.tardin@wds.co', :commits => 89},
+                {:name =>'Pau Folque', :email => 'pau.folque@wds.co', :commits => 88},
+                {:name => 'Kingsley Bickle', :email => 'kingsley.bickle@wds.co', :commits => 87}
+            ]
         end
     end
 end
