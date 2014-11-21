2d12b1fa0a752245360b6071653b00f8d10d176f
Alejandro Tardín <alejandro@tardin.com>
2014-11-21 10:10:41 +0000

    Added more sample developers to the heroes plugins

--- lib/wallboard/plugins/heroes/plugin.rb
+++ lib/wallboard/plugins/heroes/plugin.rb
@@ -6,7 +6,10 @@ module Heroes
             [
                 {:name => 'Alejandro Tardin', :email => 'alejandro.tardin@wds.co', :commits => 89},
                 {:name =>'Pau Folque', :email => 'pau.folque@wds.co', :commits => 88},
-                {:name => 'Kingsley Bickle', :email => 'kingsley.bickle@wds.co', :commits => 87}
+                {:name => 'Kingsley Bickle', :email => 'kingsley.bickle@wds.co', :commits => 87},
+                {:name => 'Phil Riley', :email => 'phil.riley@wds.co', :commits => 85},
+                {:name => 'Adina Petrean', :email => 'adina.petrean@wds.co', :commits => 77},
+                {:name => 'Fran Mendoza', :email => 'fran.mendoza@wds.co', :commits => 75}
             ]
         end
     end
