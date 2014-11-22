ed9c16d61011bd19c41385486b27435642d715d0
Alejandro Tardín <alejandro@tardin.com>
2014-11-22 23:24:43 +0000

    Added responsiveness to the heroes plugin

--- lib/wallboard/plugins/heroes/plugin.rb
+++ lib/wallboard/plugins/heroes/plugin.rb
@@ -5,11 +5,12 @@ module Heroes
         def get
             [
                 {:name => 'Alejandro Tardin', :email => 'alejandro.tardin@wds.co', :commits => 89},
-                {:name =>'Pau Folque', :email => 'pau.folque@wds.co', :commits => 88},
+                {:name => 'Pau Folque', :email => 'pau.folque@wds.co', :commits => 88},
                 {:name => 'Kingsley Bickle', :email => 'kingsley.bickle@wds.co', :commits => 87},
                 {:name => 'Phil Riley', :email => 'phil.riley@wds.co', :commits => 85},
                 {:name => 'Adina Petrean', :email => 'adina.petrean@wds.co', :commits => 77},
-                {:name => 'Fran Mendoza', :email => 'fran.mendoza@wds.co', :commits => 75}
+                {:name => 'Fran Mendoza', :email => 'fran.mendoza@wds.co', :commits => 75},
+                {:name => 'Matt Alner', :email => 'matthew.alner@wds.co', :commits => 74}
             ]
         end
     end
